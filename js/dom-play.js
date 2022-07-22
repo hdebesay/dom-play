ef4297c5bd52ce7ac4af3c8ad09ea1a5022594a5
hdebesay <HDebesay@sea.yearup.org>
2022-07-21 21:06:33 -0700

    fixed bugs

--- js/dom-play.js
+++ js/dom-play.js
@@ -1,16 +1,44 @@
 // alert("It's working!");
 
-let actors = document.querySelectorAll(".hamlet");
 
-for (let i = 0; i < actors.length; i++){
+// for (let i = 0; i < actors.length; i++){
 
 // actors[i].style.backgroundColor = "yellow";
 // document.querySelectorAll(".hamlet").style.background = "yeloow";
 
+// }
+
+function myAlert(){
+    alert("I'm Clicked!");
+}
+
+let spans = document.querySelectorAll("#play span");
+
+console.log(spans);
+
+for(const mySpan of spans){
+    mySpan.addEventListener("click", function(ev){
+        highlightActor(mySpan.dataset.actor);
+    });
+    // alert(mySpan.dataset.actor);
+    
+}
+
+function highlightActor(actor){
+    
+    for(const mySpan of spans){
+        if(actor == mySpan.dataset.actor){//current actor, highlight
+            mySpan.style.backgroundColor = 'yellow';
+
+        }else{//not current actor, remove highlight
+            mySpan.style.backgroundColor = 'white';
+        }
+    }
+    
 }
 
 function highlight(el){
-    if(el.style.backgroundColor == "yellow"){
+    if( el.style.backgroundColor == "yellow"){
         el.style.backgroundColor = 'white';
 
     }else{
